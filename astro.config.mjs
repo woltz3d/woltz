2ec1a80ac43c041237106ba6e4e38f337457892d
woltz3d <291935797+woltz3d@users.noreply.github.com>
2026-06-29 20:21:03 -0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
-// astro.config.mjs
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   output: 'static',
+  integrations: [tailwind()]
 });
